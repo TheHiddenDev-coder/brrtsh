48f5f06d7a47b078708e1ed3b5d27e0336e6f314
TheHiddenDev-coder <ericboboiciov@gmail.com>
2026-06-24 12:57:32 +0300

    Added EXIT command functionality

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,29 @@
+#![allow(unused)]
+
+use std::io::{self, Write};
+
 fn main() {
     print_title();
+
+    loop {
+        let mut cmd: String = String::new();
+
+        print!(">>> ");
+        io::stdout().flush().expect("Failed to flush"); // TODO: add actual error handling
+        io::stdin()
+            .read_line(&mut cmd)
+            .expect("Failed to read line"); // TODO: add actual error handling
+
+        let cmd = match cmd.as_str() {
+            "exit" => Commands::Exit,
+            _ => Commands::Unknown,
+        };
+
+        match cmd {
+            Commands::Exit => break,
+            _ => println!("Unknown Command"),
+        }
+    }
 }
 
 fn print_title() {
@@ -8,4 +32,12 @@ fn print_title() {
     println!("|  _ \\| |_) | |_) || | \\___ \\| |_| |");
     println!("| |_) |  _ <|  _ < | |  ___) |  _  |");
     println!("|____/|_| \\_\\_| \\_\\|_| |____/|_| |_|")
+}
+
+enum Commands {
+    Exit,
+    Echo,
+    MakeFile,
+    ReadFile,
+    Unknown,
 }
