af4e3491da745dc5edb48b4c91c4c12907672b56
TheHiddenDev-coder <ericboboiciov@gmail.com>
2026-06-24 12:29:42 +0300

    Added/modified doc comments

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,11 @@
+//! This is a shell. I think.
+//! Anyhow, I'm writing this as a learning experience, and I'm definitely having idotically large amount of fun
+//! Expected commmands:
+//! - echo -> prints to the screen
+//! - exit -> exits program
+//! - mkfile -> creates file
+//! - readfile -> reads contents of file
+
 fn main() {
     println!("Hello, world!");
 }
